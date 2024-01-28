e6904b6a7c32371038ab8834a1e9baee3fc30e5a
elvin.qiu <elvin.qiu@anker-in.com>
2024-01-28 13:56:43 +0800

    feat:修改获取的消息

--- service/msg_service.go
+++ service/msg_service.go
@@ -2,30 +2,46 @@ package service
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"io"
 	"net/http"
 )
 
+type SendMessageReq struct {
+	XMLName      xml.Name `xml:"xml"`
+	ToUserName   string   `xml:"ToUserName"`
+	FromUserName string   `xml:"FromUserName"`
+	CreateTime   int64    `xml:"CreateTime"`
+	MsgType      string   `xml:"MsgType"`
+	Content      string   `xml:"Content"`
+	MsgId        int64    `xml:"MsgId"`
+	MsgDataId    string   `xml:"MsgDataId"`
+	Idx          string   `xml:"Idx"`
+}
+
 // SendMsgHandler 消息发送管理
 func SendMsgHandler(w http.ResponseWriter, r *http.Request) {
-	// 读取内容
-	content, err := io.ReadAll(r.Body)
+	// 读取请求体
+	body, err := io.ReadAll(r.Body)
+
+	// 解析XML
+	var sendMessageReq SendMessageReq
+	err = xml.Unmarshal(body, &sendMessageReq)
 	if err != nil {
-		// 处理错误
-		fmt.Println("读取错误:", err)
-		return
 	}
+
+	sendMessageReqStr, err := json.Marshal(sendMessageReq)
 
 	res := &JsonResult{}
 	res.Code = 0
-	res.Data = "成功接收到消息" + string(content)
+	res.Data = "成功接收到消息" + string(sendMessageReqStr)
 	msg, err := json.Marshal(res)
 	if err != nil {
 		fmt.Fprint(w, "内部错误")
 		return
 	}
-	fmt.Println("接收到消息:", string(content))
+	fmt.Println("fmt接收到消息:", sendMessageReqStr)
 	w.Header().Set("content-type", "application/json")
 	w.Write(msg)
 }
