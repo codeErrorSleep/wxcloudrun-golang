63b9e471f7947599a68960d2a1185fc9f360725e
elvin.qiu <elvin.qiu@anker-in.com>
2024-01-28 15:02:55 +0800

    test:转换为json格式的

--- service/msg_service.go
+++ service/msg_service.go
@@ -2,24 +2,25 @@ package service
 
 import (
 	"encoding/json"
-	"encoding/xml"
 	"fmt"
 	"io"
 	"net/http"
 )
 
-type SendMessageReq struct {
-	XMLName xml.Name `xml:"xml"`
+// start_ai_generated
 
-	ToUserName   string `xml:"ToUserName"`
-	FromUserName string `xml:"FromUserName"`
-	CreateTime   int64  `xml:"CreateTime"`
-	MsgType      string `xml:"MsgType"`
-	Content      string `xml:"Content"`
-	MsgId        int64  `xml:"MsgId"`
-	MsgDataId    string `xml:"MsgDataId"`
-	Idx          string `xml:"Idx"`
+type SendMessageReq struct {
+	ToUserName   string `xml:"ToUserName" json:"ToUserName"`
+	FromUserName string `xml:"FromUserName" json:"FromUserName"`
+	CreateTime   int64  `xml:"CreateTime" json:"CreateTime"`
+	MsgType      string `xml:"MsgType" json:"MsgType"`
+	Content      string `xml:"Content" json:"Content"`
+	MsgId        int64  `xml:"MsgId" json:"MsgId"`
+	MsgDataId    string `xml:"MsgDataId" json:"MsgDataId"`
+	Idx          string `xml:"Idx" json:"Idx"`
 }
+
+// end_ai_generated
 
 // SendMsgHandler 消息发送管理
 func SendMsgHandler(w http.ResponseWriter, r *http.Request) {
@@ -31,54 +32,23 @@ func SendMsgHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	fmt.Println("读取请求体成功", string(body))
 
-	// 输出请求体
-	fmt.Println("请求体内容:", string(body))
-
-	// 尝试解析为XML
-	var sendMessageReq SendMessageReq
-	errXML := xml.Unmarshal(body, &sendMessageReq)
-	if errXML == nil {
-		fmt.Println("解析为XML成功:", sendMessageReq)
-		return
-	}
-	fmt.Println("解析为XML失败:", errXML)
-
 	// 尝试解析为JSON
-	var sendMessageReqJSON map[string]interface{}
-	errJSON := json.Unmarshal(body, &sendMessageReqJSON)
-	if errJSON == nil {
-		fmt.Println("解析为JSON成功:", sendMessageReqJSON)
+	var msgReq SendMessageReq
+	errJSON := json.Unmarshal(body, &msgReq)
+	if errJSON != nil {
+		fmt.Println("解析为JSON失败:", errJSON)
 		return
 	}
-	fmt.Println("解析为JSON失败:", errJSON)
-
-	// 如果都失败了，返回错误
-	fmt.Println("未知的请求格式")
-
-	// // 解析XML
-	// var sendMessageReq SendMessageReq
-	// err = xml.Unmarshal(body, &sendMessageReq)
-	// if err != nil {
-	// 	fmt.Println("结构体:解析XML失败", err)
-	// }
-
-	// var sendMessageReqV2 map[string]interface{}
-	// err = xml.Unmarshal(body, &sendMessageReqV2)
-	// if err != nil {
-	// 	fmt.Println("map:解析XML失败", err)
-	// }
-
-	sendMessageReqStr, err := json.Marshal(sendMessageReq)
+	fmt.Println("解析为JSON成功:", msgReq)
 
 	res := &JsonResult{}
 	res.Code = 0
-	res.Data = "成功接收到消息" + string(sendMessageReqStr)
+	res.Data = "成功接收到消息" + string(msgReq.Content)
 	msg, err := json.Marshal(res)
 	if err != nil {
 		fmt.Fprint(w, "内部错误")
 		return
 	}
-	fmt.Println("fmt接收到消息:", sendMessageReqStr)
 	w.Header().Set("content-type", "application/json")
 	w.Write(msg)
 }
