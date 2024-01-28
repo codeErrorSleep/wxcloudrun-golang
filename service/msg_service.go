3f4679aec3cefb70d607dfe935fddfc15fa082e3
elvin.qiu <elvin.qiu@anker-in.com>
2024-01-28 15:09:47 +0800

    feat:添加xml回复

--- service/msg_service.go
+++ service/msg_service.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"io"
 	"net/http"
@@ -19,11 +20,21 @@ type SendMessageReq struct {
 	MsgDataId    string `xml:"MsgDataId" json:"MsgDataId"`
 	Idx          string `xml:"Idx" json:"Idx"`
 }
+type ReplyMsgRes struct {
+	ToUserName   string `xml:"ToUserName"`
+	FromUserName string `xml:"FromUserName"`
+	CreateTime   int64  `xml:"CreateTime"`
+	MsgType      string `xml:"MsgType"`
+	Content      string `xml:"Content"`
+	// 若不标记XMLName, 则解析后的xml名为该结构体的名称
+	XMLName xml.Name `xml:"xml"`
+}
 
 // end_ai_generated
 
 // SendMsgHandler 消息发送管理
 func SendMsgHandler(w http.ResponseWriter, r *http.Request) {
+	// 接收请求
 	// 读取请求体
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
@@ -41,14 +52,32 @@ func SendMsgHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Println("解析为JSON成功:", msgReq)
 
-	res := &JsonResult{}
-	res.Code = 0
-	res.Data = "成功接收到消息" + string(msgReq.Content)
-	msg, err := json.Marshal(res)
+	// 这里测试 回复消息
+	replyMsg := ReplyMsgRes{
+		ToUserName:   msgReq.FromUserName,
+		FromUserName: msgReq.ToUserName,
+		CreateTime:   msgReq.CreateTime,
+		MsgType:      "text",
+		Content:      "你好，我是机器人:" + msgReq.Content,
+	}
+
+	msg, err := xml.Marshal(&replyMsg)
 	if err != nil {
-		fmt.Fprint(w, "内部错误")
+		fmt.Println("xml.Marshal失败:", err)
 		return
 	}
-	w.Header().Set("content-type", "application/json")
+
+	w.Header().Set("content-type", "application/xml")
 	w.Write(msg)
+
+	// res := &JsonResult{}
+	// res.Code = 0
+	// res.Data = "成功接收到消息" + string(msgReq.Content)
+	// msg, err := json.Marshal(res)
+	// if err != nil {
+	// 	fmt.Fprint(w, "内部错误")
+	// 	return
+	// }
+	// w.Header().Set("content-type", "application/json")
+	// w.Write(msg)
 }
